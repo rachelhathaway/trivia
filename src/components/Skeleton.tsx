dbe5b3e1ae56d16742751a7ed962c893c0b7709e
Rachel Hathaway <rachel.e.hathaway@gmail.com>
2023-07-14 22:23:47 -0500

    add Skeleton component

---
+++ src/components/Skeleton.tsx
@@ -0,0 +1,30 @@
+import { Grid, Skeleton as MuiSkeleton } from "@mui/material";
+
+export const Skeleton = () => {
+  return (
+    <Grid container direction="column" spacing={2}>
+      <Grid item xs={4}>
+        <MuiSkeleton variant="rectangular" width={245} height={20} />
+      </Grid>
+      <Grid item xs={4}>
+        <Grid container direction="column" spacing={2}>
+          {Array.from(new Array(3)).map(() => (
+            <Grid item xs={12}>
+              <Grid container spacing={2}>
+                <Grid item>
+                  <MuiSkeleton variant="circular" width={20} height={20} />
+                </Grid>
+                <Grid item>
+                  <MuiSkeleton variant="rectangular" width={210} height={20} />
+                </Grid>
+              </Grid>
+            </Grid>
+          ))}
+        </Grid>
+      </Grid>
+      <Grid item xs={4}>
+        <MuiSkeleton variant="rectangular" width={245} height={20} />
+      </Grid>
+    </Grid>
+  );
+};
