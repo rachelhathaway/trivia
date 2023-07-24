c65a9101c158458bf902dfd59438b9e4f18d9ad2
Rachel Hathaway <rachel.e.hathaway@gmail.com>
2023-07-24 18:59:54 -0500

    remove pagination row from skeleton and center content

--- src/components/Skeleton.tsx
+++ src/components/Skeleton.tsx
@@ -2,7 +2,7 @@ import { Grid, Skeleton as MuiSkeleton } from "@mui/material";
 
 export const Skeleton = () => {
   return (
-    <Grid container direction="column" spacing={2}>
+    <Grid alignItems="center" container direction="column" spacing={2}>
       <Grid item xs={4}>
         <MuiSkeleton variant="rectangular" width={245} height={20} />
       </Grid>
@@ -21,9 +21,6 @@ export const Skeleton = () => {
             </Grid>
           ))}
         </Grid>
-      </Grid>
-      <Grid item xs={4}>
-        <MuiSkeleton variant="rectangular" width={245} height={20} />
       </Grid>
     </Grid>
   );
